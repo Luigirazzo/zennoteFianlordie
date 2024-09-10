b24133aed50f022441e335b3354e5f340c857bb0
Luigirazzo <105380674+Luigirazzo@users.noreply.github.com>
2024-09-10 15:19:37 +0200

    "tryed to fix client routing problems"

--- src/index.js
+++ src/index.js
@@ -96,23 +96,7 @@ return(
 }
 function main(){
 root.render(
-  <HashRouter>
-  <nav>
-                <ul>
-                    <li>
-                        <Link to="/dashboard">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/edit">About</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Routes>
-            <Route exact path='/' element={<Load_landingpage />}></Route>
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/edit" element={<Edit/>} />
-            </Routes>
-</HashRouter>
+  <Load_landingpage/>
   
 )
 }
